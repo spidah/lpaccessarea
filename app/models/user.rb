33566c64cdb17e1924d3f01e164a95d2dc5d74ea
David Hayward <spidahman@gmail.com>
2015-08-02 22:02:51 +0100

    Time out users after 6 hours of no activity.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :validatable, :trackable
+  devise :database_authenticatable, :registerable, :validatable, :trackable, :timeoutable
   belongs_to :role
   
   def admin?

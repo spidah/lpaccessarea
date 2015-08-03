788e7971999499bbdbd6d3c2019d47f68a3727da
David Hayward <spidahman@gmail.com>
2015-08-03 19:34:18 +0100

    Load, or create a new object, the timesheet for the day.

--- app/controllers/timesheet_controller.rb
+++ app/controllers/timesheet_controller.rb
@@ -1,2 +1,17 @@
 class TimesheetController < ApplicationController
+	before_filter :authenticate_user!
+	before_filter :load_and_check_data
+	
+	protected
+	
+	def load_and_check_data
+		@timesheet = Timesheet.find_by for_date: Date.today
+		
+		if @timesheet == nil
+			@timesheet = Timesheet.new
+			@timesheet.for_date = Date.today
+			@timesheet.start_time = current_user.current_sign_in_at.to_date == Date.today ? current_user.current_sign_in_at : DateTime.current
+			@timesheet.save
+		end
+	end
 end

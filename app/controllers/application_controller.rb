50622cdb4c86508d64babcac271d033ad78258eb
David Hayward <spidahman@gmail.com>
2015-08-03 14:39:51 +0100

    Changes to spaces/tabs.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,30 +8,30 @@ class ApplicationController < ActionController::Base
   before_action :auth_user
 
   def auth_user
-    if params[:controller] != "home" && params[:controller] != "devise/sessions"
-      redirect_to root_path if !user_signed_in?
-    end
+	if params[:controller] != "home" && params[:controller] != "devise/sessions"
+	  redirect_to root_path if !user_signed_in?
+	end
   end
   
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   def after_sign_in_path_for(resource)
-    if resource.sign_in_count == 1
-      edit_user_registration_path
-    else
-      profile_index_path
-    end
+	if resource.sign_in_count == 1
+	  edit_user_registration_path
+	else
+	  timesheet_path
+	end
   end
   
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:login, :email, :password, :password_confirmation, :name, :role_id) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :password) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:login, :email, :password, :password_confirmation, :current_password, :name, :role_id, :changepassword) }
+	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:login, :email, :password, :password_confirmation, :name, :role_id) }
+	devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :password) }
+	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:login, :email, :password, :password_confirmation, :current_password, :name, :role_id, :changepassword) }
   end
   
   def authorize_admin
-    redirect_to profile_index_path unless current_user.role.admin?
+	redirect_to profile_index_path unless current_user.role.admin?
   end
 end
